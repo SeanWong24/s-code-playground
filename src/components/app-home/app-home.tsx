365718b480ee20b2ae50cc0844b7bc73fd5ed1b4
Sean Wong <seanwong24@live.com>
2021-08-28 16:05:48 -0600

    initialize the project

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,23 @@
+import { Component, h, Host } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+})
+export class AppHome {
+  render() {
+    return (
+      <Host>
+        <ion-header>
+          <ion-toolbar color="primary">
+            <ion-title>Home</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content class="ion-padding">
+          Home works.
+        </ion-content>
+      </Host>
+    );
+  }
+}
